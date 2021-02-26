af4cf57d3345a6c613d4acf4889e9420267ea148
pavle <markovic.pavle@outlook.com>
2021-02-26 12:29:48 +0100

    Changed script to do corpus transformation in paralel. Added to calculate tokens counts and to merge counts.

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,10 @@
 import os
 import time
+import json
 import logging
 from lxml import etree
+import concurrent.futures
+from collections import defaultdict
 
 
 fence = '=' * 20
@@ -11,6 +14,8 @@ CORPUS_PATH = os.path.join(os.path.dirname(os.getcwd()), "corpus")
 DATA_PATH = os.path.join(os.getcwd(), "data")
 MIN_LENGTH = 6
 
+NUM_TOKEN = "NUM"
+
 
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", 
                     filename="preprocessing.log",
@@ -18,26 +23,40 @@ logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s",
                     level=logging.INFO, 
                     datefmt="%d-%b-%y %H:%M:%S")
 
-def parse(iterator):
+def parse(iterator, dist):
     sent = list()
 
     for i in iterator:
         for line in i.split('\n'):
             tokens = line.split('\t')
-            if len(tokens) > 1 and tokens[1].isalnum():
-                sent.append(tokens[1])
-    sent.append('. ')
 
-    return len(sent), ' '.join(sent)
+            # Discard </g> tags
+            if len(tokens) < 2:
+                continue
 
-logging.info(f"{fence}Program started{fence}")
-program_start_time = time.time()
+            token = tokens[1]
 
-for corpus in CORPORA:
+            # Include only numeric or alphabetic tokens
+            # Map all numeric tokens to the same value
+            if token.isnumeric():
+                token = NUM_TOKEN
+            elif not token.isalpha():
+                continue
+
+            sent.append(token)
+            dist[token] += 1
+
+    return ' '.join(sent)
+
+
+def parse_corpus(corpus_name):
+    print(f"Processing {corpus_name}...")
     corpus_start_time = time.time()
-    context = etree.iterparse(os.path.join(CORPUS_PATH, corpus), events=("start", "end"))
-    corpus_id = corpus[:-4]
+
+    context = etree.iterparse(os.path.join(CORPUS_PATH, corpus_name), events=("start", "end"))
+    corpus_id = corpus_name[:-4]
     data_dir = os.path.join(DATA_PATH, corpus_id)
+    tokens_dist = defaultdict(float)
 
     if not os.path.exists(data_dir):
         os.mkdir(data_dir)
@@ -45,14 +64,62 @@ for corpus in CORPORA:
     with open(f"{os.path.join(data_dir, corpus_id)}.txt", 'a') as txt_file:
         for event, element in context:
             if event == "start" and element.tag == 's':
-                sent_len, sent = parse(element.itertext())
-                if sent_len >= MIN_LENGTH:
-                    txt_file.write(sent)
+                sent = parse(element.itertext(), tokens_dist)
+                txt_file.write(f"{sent}\n")
             elif event == "end":
                 element.clear()
     
-    logging.info(f"Corpus {corpus_id} time: {round(time.time() - corpus_start_time, 2)}s")
+    with open(f"{os.path.join(data_dir, corpus_id)}_token_dist.json", 'w') as json_file:
+        json.dump(tokens_dist, json_file, indent=4)
+    
+    print(f"Corpus {corpus_id} total time: {round(time.time() - corpus_start_time, 2)}s")
 
-logging.info(f"Total time: {round(time.time() - program_start_time, 2)}s")
-logging.info(f"{fence}Program finished{fence}")
 
+def transform_corpora():
+    start_transform_time = time.time()
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = executor.map(parse_corpus, CORPORA)
+
+    logging.info(f"Total transformation time: {round(time.time() - start_transform_time, 2)}s")
+
+
+def merge_tokens_dist():
+    print("Start merging tokens distributions...")
+    merging_start_time = time.time()
+    total_tokens_dist = defaultdict(float)
+
+    for dir in os.listdir(DATA_PATH):
+        dir_path = os.path.join(DATA_PATH, dir)
+
+        if not os.path.isdir(dir_path):
+            continue
+        
+        for file in os.listdir(dir_path):
+            if file.endswith("_token_dist.json"):
+                with open(f"{os.path.join(dir_path, file)}", 'r') as json_file:
+                    tokens_dist = json.load(json_file)
+
+                    for key, value in tokens_dist.items():
+                        total_tokens_dist[key] += value
+
+    with open(f"{os.path.join(DATA_PATH, 'tokens_dist.json')}", 'w') as json_file:
+        json.dump(total_tokens_dist, json_file, indent=4)
+
+    logging.info(f"Total merging time: {round(time.time() - merging_start_time, 2)}s")
+    print("Finished merging tokens distributions!")
+
+
+def create_train_sets():
+    pass
+
+
+if __name__ == "__main__":
+    logging.info(f"{fence}Program started{fence}")
+    program_start_time = time.time()
+
+    transform_corpora()
+    merge_tokens_dist()
+
+    logging.info(f"Total time: {round(time.time() - program_start_time, 2)}s")
+    logging.info(f"{fence}Program finished{fence}")
