23988770af71f716898a033d2b2cda066eb6c826
pavle <markovic.pavle@outlook.com>
2021-03-14 11:53:37 +0100

    Added jupyter notebook for visualizing embeddings.

--- src/train_gensim.py
+++ src/train_gensim.py
@@ -1,4 +1,4 @@
-from gensim import models
+from gensim.models import Word2Vec
 
 
 class ReadCorpus:
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     
     corpus_reader = ReadCorpus()
 
-    gen_model = models.Word2Vec(sentences=corpus_reader, size=300, window=5, min_count=0)
+    gen_model = Word2Vec(sentences=corpus_reader, size=300, window=5, min_count=0)
 
     gen_model.save("/content/gdrive/MyDrive/Colab_Notebooks/gen_sg300.txt")
